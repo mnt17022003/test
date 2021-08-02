39603b91ad4132e77a03a3c82b792312610b172e
mnt17022003 <88274261+mnt17022003@users.noreply.github.com>
2021-08-02 08:33:43 +0700

    Được tạo bằng Colaboratory

---
+++ tung_py.py
@@ -0,0 +1,55 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "tung.py",
+      "provenance": [],
+      "toc_visible": true,
+      "authorship_tag": "ABX9TyMk7iwwCnhqKv6EcHGpLmvp",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/mnt17022003/test/blob/main/tung_py.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "wODzg1vCfZAe",
+        "outputId": "674e2927-c703-402e-98e1-b2c77373fe72"
+      },
+      "source": [
+        "print('hello')"
+      ],
+      "execution_count": 2,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "hello\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    }
+  ]
+}
